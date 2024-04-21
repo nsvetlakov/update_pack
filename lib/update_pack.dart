2d6d83954e90dcfc3588eb69e40d052b6c87cb0f
nsvetlakov <mensaje42@gmail.com>
2024-04-21 20:40:24 +0200

    code: первый нах )))

---
+++ lib/update_pack.dart
@@ -0,0 +1,18 @@
+library update_pack;
+
+import 'package:flutter/material.dart';
+
+/// A Calculator.
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 150,
+      color: Colors.red,
+      child: const Text('update_pack'),
+    );
+  }
+}
